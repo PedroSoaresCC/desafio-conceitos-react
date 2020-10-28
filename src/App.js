e2a1a5e483715a4c1a0a4b3587d9313a971321a4
Pedro <pedro.soares@possible.com>
2020-10-28 10:57:47 -0300

    Add functionality to add and remove repository

--- src/App.js
+++ src/App.js
@@ -1,30 +1,45 @@
-import React from "react";
+import React , { useState, useEffect } from "react";
+import api from './services/api'
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setrepositories] = useState([]);
+
   async function handleAddRepository() {
-    // TODO
+      const response = await api.post('repositories', {
+          title: `Novo projeto ${Date.now()}`,
+          url: "url",
+          techs: ['react', 'angular ']
+        
+      })
+      
+      setrepositories([...repositories, response.data]);
   }
 
+  useEffect(() => {
+      api.get('repositories').then(response => {
+          setrepositories(response.data)
+      })
+  }, [repositories])
+
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`)
   }
 
   return (
-    <div>
+    <>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map((repository) => (
+            <li key={repository.id}>
+              {repository.title} 
+              <button onClick={() => handleRemoveRepository(repository.id)}>Remover</button>
+            </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
-    </div>
+    </>
   );
 }
 

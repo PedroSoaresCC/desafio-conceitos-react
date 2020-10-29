fc1124bbab83f34547c67b2e43a69a247b299244
Pedro <pedro.soares@possible.com>
2020-10-29 20:55:30 -0300

    Adjustments on code

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ import "./styles.css";
 function App() {
   const [repositories, setrepositories] = useState([]);
 
+  useEffect(() => {
+      api.get('repositories').then(response => {
+          setrepositories(response.data)
+      })
+  }, [])
+
   async function handleAddRepository() {
       const response = await api.post('repositories', {
           title: `Novo projeto ${Date.now()}`,
@@ -17,29 +23,27 @@ function App() {
       setrepositories([...repositories, response.data]);
   }
 
-  useEffect(() => {
-      api.get('repositories').then(response => {
-          setrepositories(response.data)
-      })
-  }, [repositories])
-
+ 
   async function handleRemoveRepository(id) {
     await api.delete(`repositories/${id}`)
+
+    setrepositories(repositories.filter(repository => repository.id !== id))
   }
 
   return (
-    <>
+    <div>
       <ul data-testid="repository-list">
-        {repositories.map((repository) => (
+        {repositories.map(repository => (
             <li key={repository.id}>
-              {repository.title} 
+              {repository.title}
+
               <button onClick={() => handleRemoveRepository(repository.id)}>Remover</button>
             </li>
         ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
-    </>
+    </div>
   );
 }
 
